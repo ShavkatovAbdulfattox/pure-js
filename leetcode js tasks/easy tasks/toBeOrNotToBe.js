6ffc2712567d3e4baf1fca37855d5c60ebf566e0
Abdulfattoh <vwhoami@yahoo.com>
2023-09-25 14:36:49 +0500

    tobeOrNotTobe is solved

---
+++ leetcode js tasks/easy tasks/toBeOrNotToBe.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  return {
+    toBe: (val2) => {
+      if (val === val2) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: (val3) => {
+      if (val !== val3) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    },
+  };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
